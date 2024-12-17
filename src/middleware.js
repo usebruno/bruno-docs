5a416f931b1c441ac836f373891bc21b2467a716
ganesh-bruno <ganesh@usebruno.com>
2024-12-17 13:21:01 +0530

    add redirect links

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,13 @@ import { NextResponse } from "next/server";
 const redirects = {
   "/introduction/getting-started": "/introduction/what-is-bruno",
   "/license-management/license-management": "/license-management/overview",
+
+  "/testing/assertions": "/testing/tests/assertions",
+
+  "/scripting/request/request-object": "/testing/script/request/request-object",
+
+  "/scripting/response/response-object":
+    "/testing/script/response/response-object",
 };
 
 export function middleware(request) {
