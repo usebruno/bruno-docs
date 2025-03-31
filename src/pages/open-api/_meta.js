6c20406a45b4d250a22bf334589e68e2326dde36
ganesh-bruno <ganesh@usebruno.com>
2025-03-31 16:30:44 +0530

    OAS spec revamp

--- src/pages/open-api/_meta.js
+++ src/pages/open-api/_meta.js
@@ -1,3 +1,6 @@
 export default {
-  "overview": "Overview"
-} 
+  "overview": "Overview",
+  "createOAS": "Create an OpenAPI Spec",
+  "importOAS": "Import an OpenAPI Spec",
+  "exportOAS": "Export a Collection as OpenAPI Spec"
+}
