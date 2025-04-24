2d83bc79c37934082efb0ba2d9b6f3ebc520172e
ganesh-bruno <ganesh@usebruno.com>
2025-04-24 12:29:26 +0530

    revamp converter section

---
+++ src/pages/converters/_meta.js
@@ -0,0 +1,6 @@
+export default {
+    "overview": "Overview",
+    "bruno-to-postman": "Bruno to Postman",
+    "bruno-to-insomnia": "Bruno to Insomnia",
+    "bruno-to-openapi": "Bruno to OpenAPI"
+}
