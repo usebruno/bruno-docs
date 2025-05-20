97bef33b3b225366328651a17556cb92c9548ffa
ganesh-bruno <ganesh@usebruno.com>
2025-05-20 17:53:54 +0530

    postman envuronment import

--- src/pages/get-started/import-export-data/_meta.js
+++ src/pages/get-started/import-export-data/_meta.js
@@ -2,6 +2,5 @@ export default {
   "import-collections": "Import Collections",
   "export-collections": "Export Collections",
   "postman-migration": "Postman Migration",
-  "import-postman-environment": "Import Postman Environment",
   "script-translator": "Script Translator"
 } 
