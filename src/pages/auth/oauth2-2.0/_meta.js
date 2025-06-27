dd17c2cc229d952c8607b1469488d226893dbe42
ganesh-bruno <ganesh@usebruno.com>
2025-06-27 15:11:04 +0530

    added docs for folder level auth

--- src/pages/auth/oauth2-2.0/_meta.js
+++ src/pages/auth/oauth2-2.0/_meta.js
@@ -1,6 +1,7 @@
 export default {
   "overview": "Overview",
   "collection-level-configuration": "Collection Level Configuration",
+  "folder-level-configuration": "Folder Level Configuration",
   "request-level-configuration": "Request Level Configuration",
   "authorization-code": "Authorization Code",
   "client-credentials": "Client Credentials",
