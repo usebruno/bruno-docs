7f62e9a86250c73946079e7b01a72a5dce033f8d
ganesh-bruno <ganesh@usebruno.com>
2025-07-02 14:53:00 +0530

    removed request level doc

--- src/pages/auth/oauth2-2.0/_meta.js
+++ src/pages/auth/oauth2-2.0/_meta.js
@@ -1,8 +1,6 @@
 export default {
   "overview": "Overview",
-  "collection-level-configuration": "Collection Level Configuration",
-  "folder-level-configuration": "Folder Level Configuration",
-  "request-level-configuration": "Request Level Configuration",
+  "collection-level-configuration": "OAuth 2.0",
   "authorization-code": "Authorization Code",
   "client-credentials": "Client Credentials",
   "password-credentials": "Password Credentials"
