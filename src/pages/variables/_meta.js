4609cc2e8379ec93e605f9378d80e42f8f70fdae
Bobby Bonestell <bobby@preczn.com>
2025-07-16 11:21:07 -0600

    Cleaning up whitespace changes

--- src/pages/variables/_meta.js
+++ src/pages/variables/_meta.js
@@ -4,9 +4,9 @@ export default {
   "global-environment-variables": "Global Environment Variables",
   "collection-variables": "Collection Variables",
   "folder-variables": "Folder Variables",
-	"request-variables": "Request Variables",
-	"runtime-variables": "Runtime Variables",
-	"prompt-variables": "Prompt Variables",
+  "request-variables": "Request Variables",
+  "runtime-variables": "Runtime Variables",
+  "prompt-variables": "Prompt Variables",
   "process-env": "Process Environment Variables",
   "interpolation":"Variables Interpolation"
 } 
