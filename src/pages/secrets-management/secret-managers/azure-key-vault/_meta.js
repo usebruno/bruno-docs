2d78564fdba99ab788c41b10ec475970fb9da30e
ganesh-bruno <ganesh@usebruno.com>
2025-07-25 13:38:19 +0530

    add secrets auth cli docs

--- src/pages/secrets-management/secret-managers/azure-key-vault/_meta.js
+++ src/pages/secrets-management/secret-managers/azure-key-vault/_meta.js
@@ -1,6 +1,7 @@
 export default {
   "overview": "Overview",
   "adding-a-secret-provider": "Adding a Secret Provider",
+  "cli-authentication": "Azure CLI Authentication",
   "configuring-and-fetching-secrets": "Configuring and Fetching Secrets",
   "using-secrets": "Using Secrets"
 } 
