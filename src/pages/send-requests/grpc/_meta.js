43120beb2bc7f55aa72b8b3848c89719fdd139ee
ganesh-bruno <ganesh@usebruno.com>
2025-08-13 16:54:54 +0530

    added two sections for proto and streams

--- src/pages/send-requests/grpc/_meta.js
+++ src/pages/send-requests/grpc/_meta.js
@@ -1,4 +1,6 @@
 export default {
     "overview": "Overview",
-    "grpc-request": "gRPC Request"
+    "grpc-request": "gRPC Request",
+    "grpc-proto": "gRPC Proto",
+    "grpc-streams": "gRPC Streams"
 }
