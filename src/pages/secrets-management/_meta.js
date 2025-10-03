0eebba847b871f0282ccba7aa62c44770b369511
sreelakshmi-bruno <sreelakshmi@usebruno.com>
2025-10-03 16:42:11 +0530

    secret masking in reports

--- src/pages/secrets-management/_meta.js
+++ src/pages/secrets-management/_meta.js
@@ -2,5 +2,6 @@ export default {
   "overview": "Overview",
   "dotenv-file": "DotEnv File",
   "secret-variables": "Secret Variables",
+  "secret-masking": "Secret Masking",
   "secret-managers": "Secret Managers"
 } 
