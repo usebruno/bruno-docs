8ba561d5242de0c680b3f73357977566bc22cbfe
James Ha <james@usebruno.com>
2025-10-14 15:21:11 -0400

    entra SSO Docs - draft

--- src/pages/license-administrators/scim-provisioning/_meta.js
+++ src/pages/license-administrators/scim-provisioning/_meta.js
@@ -1,5 +1,6 @@
 export default {
   "overview": "Overview",
   "configure-scim-with-okta": "Configure SCIM with Okta",
+  "configure-scim-with-entra-id": "Configure SCIM with Entra ID",
   "bruno-scim-api": "Bruno SCIM API"
 }
