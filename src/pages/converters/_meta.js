d3f814faea6f69590a94f7f50e870ed8aaa69482
ganesh-bruno <ganesh@usebruno.com>
2025-11-04 17:44:10 +0530

    added docs for 2.14

--- src/pages/converters/_meta.js
+++ src/pages/converters/_meta.js
@@ -2,5 +2,6 @@ export default {
     "overview": "Overview",
     "postman-to-bruno": "Postman to Bruno",
     "insomnia-to-bruno": "Insomnia to Bruno",
-    "openapi-to-bruno": "OpenAPI to Bruno"
+    "openapi-to-bruno": "OpenAPI to Bruno",
+    "wsdl-to-bruno": "WSDL to Bruno"
 }
