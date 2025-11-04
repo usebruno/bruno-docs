0a1442eb1aa979277afa4c3fd046cddc07a73753
ganesh-bruno <ganesh@usebruno.com>
2025-11-04 19:43:38 +0530

    add new document as import data

--- src/pages/send-requests/res-data-cookies/_meta.js
+++ src/pages/send-requests/res-data-cookies/_meta.js
@@ -1,6 +1,9 @@
-export default {
+const meta = {
   "overview": "Overview",
   "res-data": "Response Data",
+  "response-examples": "Response Examples",
   "cookies": "Cookies",
   "debugging": "Debugging"
-} 
+};
+
+export default meta; 
