3e6685960f77c1a68b96439e6db3f2f2a6ba8ca2
ganesh-bruno <ganesh@usebruno.com>
2026-01-02 21:04:47 +0530

    oauth2 browser support and callback url

--- content/auth/oauth2-2.0/_meta.js
+++ content/auth/oauth2-2.0/_meta.js
@@ -3,5 +3,7 @@ export default {
   "collection-level-configuration": "OAuth 2.0",
   "authorization-code": "Authorization Code",
   "client-credentials": "Client Credentials",
-  "password-credentials": "Password Credentials"
+  "password-credentials": "Password Credentials",
+  "system-browser": "System Browser",
+  "callback-url": "Callback URL"
 } 
