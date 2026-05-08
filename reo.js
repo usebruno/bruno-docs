c336c649dcc3259d38474d7ddc06257bd92a0e52
rreyn-bruno <ryan@usebruno.com>
2026-05-08 11:27:26 -0500

    Add Reo tracking beacon
    - Add reo.js to enable Reo analytics tracking across all documentation pages
    - Uses client ID: e3928a3bc4be465
    - Script loads asynchronously via Mintlify's custom JS feature

---
+++ reo.js
@@ -0,0 +1,2 @@
+// Reo tracking beacon
+!function(){var e,t,n;e="e3928a3bc4be465",t=function(){Reo.init({clientID:"e3928a3bc4be465"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}();
